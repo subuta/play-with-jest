c0b3dc04eb374578e1ee83b985976b0d64d40651
subuta <shunta.3110@gmail.com>
2019-06-19 15:14:16 +0900

    Fix example.

--- src/__tests__/tax.spec.js
+++ src/__tests__/tax.spec.js
@@ -1,6 +1,12 @@
 import applyTax from '../tax'
 
 test('should calculate japanese tax.', () => {
-  expect(applyTax(100, 1.08)).toBe("¥108")
-  expect(applyTax(101, 1.08, false)).toBe("¥109")
+  expect(applyTax(100, 1.08)).toBe('¥108')
+  expect(applyTax(101, 1.08, false)).toBe('¥109')
+
+  // Min price
+  expect(applyTax(0, 1.08)).toBe('¥0')
+
+  // Max price
+  expect(applyTax(Number.MAX_SAFE_INTEGER / 1.08, 1.08)).toBe('¥9007199254740990')
 })
